45b2d6c8c8cf204f7f06fc907ea7cbf23e887d7b
Aaron <caper2711@hotmail.com>
2024-01-08 10:44:16 -0600

    Fix: Format and typos

--- second.sql
+++ second.sql
@@ -6,51 +6,100 @@ CREATE TABLE user (
     PRIMARY KEY (id)
 );
 
-INSERT INTO user (user_user_name, age, email) VALUES ('Layla',98,'layla@gmail.com');
-INSERT INTO user (user_name, age, email) VALUES ('Omar',30,'omar@gmail.com');
-INSERT INTO user (user_name, age, email) VALUES ('Aaron',21,'aaron@gmail.com');
-INSERT INTO user (user_name, age, email) VALUES ('Daniel',24,'daniel@gmail.com');
+-- Inserción de cuatro registros
+INSERT INTO user (user_name, age, email)
+VALUES ('Layla', 98, 'layla@gmail.com');
+INSERT INTO user (user_name, age, email)
+VALUES ('Omar', 30, 'omar@gmail.com');
+INSERT INTO user (user_name, age, email)
+VALUES ('Aaron', 21, 'aaron@gmail.com');
+INSERT INTO user (user_name, age, email)
+VALUES ('Daniel', 24, 'daniel@gmail.com');
+
 
 -- Limita cuántos registros retorna (si hay varios valores coincidentes, regresa el primero que encuentra)
-SELECT * FROM user LIMIT 1;
+SELECT *
+FROM user
+LIMIT 1;
+
 
 -- Condición con mayor que (se le puede agregar limit al final, si no, regresa todos los que cumplen con la condición)
 -- Aplica con >=, <, <=
-SELECT * FROM user WHERE age > 25;
- -- Condición extendida con un segundo valor 
-SELECT * FROM user WHERE age > 20 AND email = 'omar@gmail.com';
--- Operador OR
-SELECT * FROM user WHERE age = 24 OR email = 'aaron@gmail.com';
+SELECT *
+FROM user
+WHERE age > 25;
 
--- Retorna todos los correos excepto...
-SELECT * FROM user WHERE email != 'aaron@gmail.com';
+-- Condición extendida con un segundo valor --
+SELECT *
+FROM user
+WHERE age > 20
+    AND email = 'omar@gmail.com';
 
--- Retorna todos los regitros donde el valor del campo esté entre las dos condiciones
-SELECT * FROM user WHERE age BETWEEN 20 AND 30;
 
--- Indica que quiero todos los registros que contengan la cadena, sin importar su principio ni final
-SELECT * FROM user WHERE email LIKE '%gmail%'; 
--- Puede ser %gmail para que no importe lo que haya antes pero que la cadena termine con gmail
-SELECT * FROM user WHERE email LIKE '%gmail'; 
--- O gmail% para indicar que la cadena tiene que iniciar con gmail y no importa lo que haya después
-SELECT * FROM user WHERE email LIKE 'gmail%'; 
+-- Operador OR --
+SELECT *
+FROM user
+WHERE age = 24
+    OR email = 'aaron@gmail.com';
+
+
+-- Retorna todos los correos EXCEPTO...
+SELECT *
+FROM user
+WHERE email != 'aaron@gmail.com';
+
+
+-- Retorna todos los regitros donde el valor del campo esté entre el rango de las dos condiciones
+SELECT *
+FROM user
+WHERE age BETWEEN 20 AND 30;
+
+
+-- Indica que quiero todos los registros que contengan la cadena, SIN IMPORTAR SU INICIO NI FINAL
+SELECT *
+FROM user
+WHERE email LIKE '%gmail%';
+
+
+-- Puede ser %gmail para que NO IMPORTA LO QUE HAYA ANTES pero que la cadena TERMINE con gmail
+SELECT *
+FROM user
+WHERE email LIKE '%gmail';
+
+
+-- O gmail% para indicar que la cadena tiene que INICIAR CON gmail y NO IMPORTA LO QUE HAYA DESPUÉS
+SELECT *
+FROM user
+WHERE email LIKE 'gmail%';
+
 
 -- Muestra los registros en un orden específico (ASCENDENTE (ASC) O DESCENDENTE (DESC))
-SELECT * FROM user ORDER BY age ASC;
-SELECT * FROM user ORDER BY age DESC;
+SELECT *
+FROM user
+ORDER BY age ASC;
+SELECT *
+FROM user
+ORDER BY age DESC;
 
--- Seleccionar el valor máximo del campo elegido y se le asocia un alias
-SELECT MAX(age) AS mayor FROM user;
--- Seleccionar el valor mínimo del campo elegido y se le asocia un alias
-SELECT MIN(age) AS menor FROM user;
--- Después de select se usa max o min SON FUNCIONES y por eso se usan con paréntesis con el nombre de la columna dentro
--- Después se pone AS seguido del alias que queremos ponerle, al final se especifica la tabla de donde se va a sacar el dato
+
+-- Seleccionar el valor MÁXIMO del campo elegido y se le asocia un alias
+SELECT MAX(age) AS mayor
+FROM user;
+-- Seleccionar el valor MÍNIMO del campo elegido y se le asocia un alias
+SELECT MIN(age) AS menor
+FROM user;
+/* 
+ Después de select se usa max o min SON FUNCIONES y por eso se usan con paréntesis 
+ con el nombre de la columna dentro, Después se pone AS seguido del alias que 
+ queremos ponerle, al final se especifica la tabla de donde se va a sacar el dato
+*/
+
 
 -- Seleccionar solamente las columnas que nos interesan
-SELECT age, user_name FROM user;
+SELECT age,
+    user_name
+FROM user;
 -- Se le puede agregar un alias también a las columnas cuando se muestran en consola
-SELECT age, user_name as nombre FROM user; 
-
-
-
-
+SELECT age,
+    user_name AS nombre
+FROM user;
--- third.sql
+++ third.sql
@@ -1,101 +1,152 @@
 CREATE TABLE products (
-	id int NOT NULL AUTO_INCREMENT,
-    name varchar(50) NOT NULL,
+    id int NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
     created_by int NOT NULL,
-    marca varchar(50) NOT NULL,
+    marca VARCHAR(50) NOT NULL,
     PRIMARY KEY(id),
     -- La llave foránea es una ligadura a otra tabla, se referencía nombrando la tabla y la columna
     FOREIGN KEY(created_by) REFERENCES user(id)
 );
 
+
 -- Cómo renombrar una tabla
-RENAME TABLE products to product;
+RENAME TABLE products TO product;
+
 
 -- Cómo insertar múltiples registros con una sola llamada
 INSERT INTO product (name, created_by, marca)
-values
-	('ipad', 1, 'apple'),
+VALUES ('ipad', 1, 'apple'),
     ('iphone', 1, 'apple'),
     ('macbook', 1, 'apple'),
     ('applewatch', 2, 'apple'),
     ('imac', 3, 'apple'),
     ('ipad mini', 2, 'apple');
-    
+
+
+SELECT *
+FROM product 
+
 
 -- LEFT JOIN --
-/* Primero seleccionas las columnas que se van a mostrar, luego la primera tabla, luego indicas el alias(opcional), 
-   luego va el LEFT JOIN, luego el nombre de la segunda tabla, alias (opcional) y la condición para que la información 
-   complementaria de la segunda tabla se muestre (si no se cumple, se muestra NULL) 
-*/
 /* 
-   La primera tabla es la base, y la segunda complementa con su información 
-   (agrega el nombre del producto en los registros que tengan el mismo id (lo que está después del ON)) 
-   En pocas palabras: retorna toda la información de las columnas mencionadas de la primera tabla 
-   y agrega COMO EXTRA la información de la segunda tabla que cumpla con la condición después del ON 
+ Primero seleccionas las columnas que se van a mostrar, luego la primera tabla, luego indicas el alias(opcional), 
+ luego va el LEFT JOIN, luego el nombre de la segunda tabla, alias (opcional) y la condición para que la información 
+ complementaria de la segunda tabla se muestre (si no se cumple, se muestra NULL) 
+
+ La primera tabla es la base, y la segunda complementa con su información 
+ (agrega el nombre del producto en los registros que tengan el mismo id (lo que está después del ON)) 
+ En pocas palabras: retorna toda la información de las columnas mencionadas de la primera tabla 
+ y agrega COMO EXTRA la información de la segunda tabla que cumpla con la condición después del ON 
 */
-select u.id, u.user_name, p.name from user u left join product p ON u.id = p.created_by;
+SELECT u.id,
+    u.user_name,
+    p.name
+FROM user u
+    LEFT JOIN product p ON u.id = p.created_by;
+
 -- Forma alternativa sin alias a las tablas
-select user.id, user.user_name, product.name from user left join product ON user.id = product.created_by;
+SELECT user.id,
+    user.user_name,
+    product.name
+FROM user
+    LEFT JOIN product ON user.id = product.created_by;
+
 -- Fancy way cambiando los nombres de los encabezados, agregando alias para las tablas en el query y ordenando la tabla por id ascendente
-select u.id as ID, u.user_name as Productor, p.name as Producto from user u left join product p ON u.id = p.created_by order by id asc;
+SELECT u.id AS ID,
+    u.user_name AS Productor,
+    p.name AS Producto
+FROM user u
+    LEFT JOIN product p ON u.id = p.created_by
+ORDER BY id ASC;
 
 
 -- RIGHT JOIN --
 /*
-   En vez de que la tabla1 sea complementada con la tabla2, tabla2 es complementada con la tabla1 
-   (por lo tanto los valores en la tabla1 que no estén relacionados con la tabla2, no se van a mostrar)
-*/
-select user.id, user.user_name, product.name from user right join product ON user.id = product.created_by;
+ En vez de que la tabla1 sea complementada con la tabla2, tabla2 es complementada con la tabla1 
+ (por lo tanto los valores en la tabla1 que no estén relacionados con la tabla2, no se van a mostrar)
+ Se puede lograr lo mismo haciendo un left join pero volteando las tablas de lugar
+ */
+SELECT user.id,
+    user.user_name,
+    product.name
+FROM user
+    RIGHT JOIN product ON user.id = product.created_by;
+
 
 -- INNER JOIN --
 /*
-	Muestra solamente los registros que coincidan 
-*/
-select user.id, user.user_name, product.name from user inner join product ON user.id = product.created_by;
+ Muestra solamente los registros que coincidan 
+ */
+SELECT user.id,
+    user.user_name,
+    product.name
+FROM user
+    INNER JOIN product ON user.id = product.created_by;
+
 
 -- CROSS JOIN --
 /*
-	Retorna el producto cartesiano de las dos tablas (todas las filas combinadas con las filas de la otra tabla)
-    Ejemplo: tienes en una tabla 2 alumnos y en otra tabla 3 clases, cross join va a pasar esos dos alumnos por todas
-			 las clases, dando como resultado una tabla con 6 filas de cada alumno en cada clase (el mismo alumno en las
-             tres clases y el otro alumno también en las tres clases)
-    CUIDADO porque puede devolver muchísimos registros
-*/
--- No necesita especificarse un ON 
-select user.id,  user.user_name, product.id, product.name from user cross join product;
+ Retorna el producto cartesiano de las dos tablas (todas las filas combinadas con las filas de la otra tabla)
+ Ejemplo: tienes en una tabla 2 alumnos y en otra tabla 3 clases, cross join va a pasar esos dos alumnos por todas
+ las clases, dando como resultado una tabla con 6 filas de cada alumno en cada clase (el mismo alumno en las
+ tres clases y el otro alumno también en las tres clases)
+ Su utilidad está en el campo de la estadística e investigación, pudiendo obtener las combinaciones posibles
+ dentro de un conjunto de datos y potencialmente reconocer patrones.
+
+ CUIDADO porque puede devolver muchísimos registros
+ */
+-- NO necesita especificarse un ON 
+SELECT user.id,
+    user.user_name,
+    product.id,
+    product.name
+FROM user
+    CROSS JOIN product;
 
 
 -- GROUP BY --
 /*
-	Agrupar elementos por columna, se usa acompañado de count
-*/
+Agrupar elementos por columna, se usa acompañado de count
+ */
 -- Cuenta cuántos productos agregó cada id de user, para eso necesitas hacer un join y mostrar los user name
-select count(p.id), u.user_name from product p left join user u ON u.id = p.created_by group by p.created_by;
+SELECT COUNT(p.id),
+    u.user_name
+FROM product p
+    LEFT JOIN user u ON u.id = p.created_by
+GROUP BY p.created_by;
 -- Cuenta los nombres del producto que hemos agregado 
-select count(name), marca from product group by marca;
+SELECT COUNT(name),
+    marca
+FROM product
+GROUP BY marca;
+
 
 -- HAVING --
 /*
-	Se agrega al final de la query 
-	Para arrojar resultados que sean relevantes, donde su conteo sea mayor que un número (por ejemplo), 
-    aplican todas las condicionantes ya vistas.
-*/
-select count(p.id), u.user_name from product p left join user u ON u.id = p.created_by group by p.created_by having count(u.id) >= 2;
+ Se agrega al final de la query 
+ Para arrojar resultados que sean relevantes, donde su conteo sea mayor que un número (por ejemplo), 
+ aplican todas las condicionantes ya vistas.
+ */
+SELECT COUNT(p.id),
+    u.user_name
+FROM product p
+    LEFT JOIN user u ON u.id = p.created_by
+GROUP BY p.created_by
+HAVING COUNT(u.id) >= 2;
+
 
 -- DROP TABLE--
 /*
-	Para eliminar una tabla
-*/
-
-drop table product;
+ Para eliminar una tabla
+ */
+DROP TABLE product;
 -- Para eliminar varias tablas
-drop table animales, user;
+DROP TABLE animales,
+user;
 
 
-
-
-
-
-
-
-
+-- DROP DATABASE --
+/*
+ Para eliminar la base de datos completa
+*/
+DROP DATABASE user;
